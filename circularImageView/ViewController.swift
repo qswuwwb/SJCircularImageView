9513971a9c492316e980e5b3bcd8fed573ee5c12
qswuwwb <qswuw@sina.com>
2016-02-01 01:38:47 +0800

    Initial Commit

---
+++ circularImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  circularImageView
+//
+//  Created by ysj on 16/2/1.
+//  Copyright © 2016年 newbieYin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
